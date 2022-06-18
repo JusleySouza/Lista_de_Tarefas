c4bcdb324a2bd524bb8cb103c6e5f0ac9afca15a
Jusley Souza <jusleysouza31@gmail.com>
2022-06-18 20:00:09 -0300

    Inicializando o projeto

---
+++ src/main/java/com/task/list/TasListApplication.java
@@ -0,0 +1,15 @@
+//parte inicial do projeto (main)
+
+package com.task.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/list/TasListApplicationTests.java
@@ -0,0 +1,15 @@
+//classe de teste
+
+package com.task.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
