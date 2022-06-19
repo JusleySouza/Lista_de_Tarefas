7458061f0b654a086cd23223164af59323c2bd8c
Jusley Souza <jusleysouza31@gmail.com>
2022-06-18 22:30:46 -0300

    Refatoração do codigo

--- src/main/java/com/task/list/repository/TaskListRepository.java
+++ src/main/java/com/task/list/repository/TaskListRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.task.list.model.TaskList;
 
 public interface TaskListRepository extends JpaRepository<TaskList, Long> {
-
-	
 	
 }
