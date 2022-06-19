b555973cd962b8f7f37e5fc998626b44a8051934
Jusley Souza <jusleysouza31@gmail.com>
2022-06-18 23:44:35 -0300

    Definição da rota de delete

--- src/main/java/com/task/list/controller/TaskListController.java
+++ src/main/java/com/task/list/controller/TaskListController.java
@@ -3,6 +3,7 @@ package com.task.list.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,4 +43,8 @@ public class TaskListController {
 		return services.update(taskList);
 	}
 	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable("id") Long id) {
+		services.delete(id);
+	}
 }
