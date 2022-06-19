6abef6c83c4369d35ef759e260ea21d6368f899f
Jusley Souza <jusleysouza31@gmail.com>
2022-06-18 23:28:22 -0300

    Definição da rota Busca por Id

--- src/main/java/com/task/list/controller/TaskListController.java
+++ src/main/java/com/task/list/controller/TaskListController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,12 @@ public class TaskListController {
 	public List<TaskList> findAll(){
 		return services.findAll();
 	}
+	
+	@GetMapping("/{id}")
+	public TaskList findById(@PathVariable("id") Long id) {
+		return services.findById(id);
+	}
+	
 	
 	@PostMapping
 	public TaskList create(@RequestBody TaskList taskList) {
