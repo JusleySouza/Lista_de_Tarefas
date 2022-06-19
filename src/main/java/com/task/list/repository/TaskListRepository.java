4cbf9df696d1893f627282c3b33b0a13187848ea
Jusley Souza <jusleysouza31@gmail.com>
2022-06-18 22:09:47 -0300

    Criação do repository

---
+++ src/main/java/com/task/list/repository/TaskListRepository.java
@@ -0,0 +1,11 @@
+package com.task.list.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.task.list.model.TaskList;
+
+public interface TaskListRepository extends JpaRepository<TaskList, Long> {
+
+	
+	
+}
