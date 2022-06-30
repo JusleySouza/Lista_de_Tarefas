1c1b9d1cfbcaaaf331bf58adc00ae56f392967a5
Jusley Souza <jusleysouza31@gmail.com>
2022-06-29 23:24:01 -0300

    Alteração do retorno dos métodos create e delete e personalização do código Http de resposta

--- src/main/java/com/task/list/services/implement/TaskListServicesImplement.java
+++ src/main/java/com/task/list/services/implement/TaskListServicesImplement.java
@@ -3,40 +3,49 @@ package com.task.list.services.implement;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
-import com.task.list.model.TaskList;
+import com.task.list.dto.TaskDTO;
+import com.task.list.model.Task;
 import com.task.list.repository.TaskListRepository;
 import com.task.list.services.TaskListServices;
 
 @Component
 public class TaskListServicesImplement implements TaskListServices {
 
+	private Task task;
+	
 	@Autowired
 	TaskListRepository repository;
 	
 	@Override
-	public List<TaskList> findAll() {
+	public List<Task> findAll() {
 		return repository.findAll();
 	}
 
 	@Override
-	public TaskList findById(Long id) {
-		return repository.findById(id).orElse(new TaskList());
+	public Task findById(Long id) {
+		return repository.findById(id).orElse(new Task());
 	}
 
 	@Override
-	public TaskList create(TaskList taskList) {
+	public ResponseEntity<Task> create(TaskDTO taskDTO) {
+		task = new Task();
+		task.transformDTOtoModel(taskDTO);
+		repository.save(task);
+		return new ResponseEntity<Task>( HttpStatus.CREATED);
+	}
+
+	@Override
+	public Task update(Task taskList) {
 		return repository.save(taskList);
 	}
 
 	@Override
-	public TaskList update(TaskList taskList) {
-		return repository.save(taskList);
-	}
-
-	@Override
-	public void delete(Long id) {
+	public  ResponseEntity<Task> delete(Long id) {
 		repository.deleteById(id);
+		return new ResponseEntity<Task>( HttpStatus.NO_CONTENT);
 	}
 }
