261ca924e84eba0c0775d80834a8a999a6e17caf
Jusley Souza <jusleysouza31@gmail.com>
2022-06-29 23:25:03 -0300

    Alteração do retorno dos métodos create e delete

--- src/main/java/com/task/list/controller/TaskListController.java
+++ src/main/java/com/task/list/controller/TaskListController.java
@@ -3,6 +3,7 @@ package com.task.list.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.task.list.model.TaskList;
+import com.task.list.dto.TaskDTO;
+import com.task.list.model.Task;
 import com.task.list.services.TaskListServices;
 
 @RestController
@@ -21,30 +23,30 @@ public class TaskListController {
 
 	@Autowired
 	TaskListServices services;
-	
+
 	@GetMapping
-	public List<TaskList> findAll(){
+	public List<Task> findAll() {
 		return services.findAll();
 	}
-	
+
 	@GetMapping("/{id}")
-	public TaskList findById(@PathVariable("id") Long id) {
+	public Task findById(@PathVariable("id") Long id) {
 		return services.findById(id);
 	}
-	
-	
+
 	@PostMapping
-	public TaskList create(@RequestBody TaskList taskList) {
-		return services.create(taskList);
+	public ResponseEntity<Task> create(@RequestBody TaskDTO taskDTO) {
+		return services.create(taskDTO);
 	}
-	
+
 	@PutMapping
-	public TaskList update(@RequestBody TaskList taskList) {
+	public Task update(@RequestBody Task taskList) {
 		return services.update(taskList);
 	}
-	
+
 	@DeleteMapping("/{id}")
-	public void delete(@PathVariable("id") Long id) {
-		services.delete(id);
+	public ResponseEntity<Task> delete(@PathVariable("id") Long id) {
+		return services.delete(id);
+
 	}
 }
