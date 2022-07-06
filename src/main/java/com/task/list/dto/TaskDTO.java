b2dedcfb70500776fa2b1134b42ef478ba97a485
Jusley Souza <jusleysouza31@gmail.com>
2022-07-05 22:57:58 -0300

    Utilizando Lombok para gerar os métodos Getter e Setter, ToString e EqualsAndHasCode

--- src/main/java/com/task/list/dto/TaskDTO.java
+++ src/main/java/com/task/list/dto/TaskDTO.java
@@ -9,6 +9,15 @@ import javax.persistence.TemporalType;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.task.list.model.Task;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class TaskDTO {
 
 	private Long id;
@@ -17,65 +26,16 @@ public class TaskDTO {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", locale = "pt-BR", timezone = "Brazil/East")
 	private Date finishDate;
 	private String description;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public Date getFinishDate() {
-		return finishDate;
-	}
-	public void setFinishDate(Date finishDate) {
-		this.finishDate = finishDate;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
 
-	@Override
-	public String toString() {
-		return "TaskDTO [id=" + id + ", name=" + name + ", finishDate=" + finishDate + ", description=" + description
-				+ "]";
-	}
-	
-	@Override
-	public int hashCode() {
-		return Objects.hash(description, finishDate, id, name);
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		TaskDTO other = (TaskDTO) obj;
-		return Objects.equals(description, other.description) && Objects.equals(finishDate, other.finishDate)
-				&& id == other.id && Objects.equals(name, other.name);
-	}
-	
 	public TaskDTO transformModelToDTO(Task task) {
 		TaskDTO taskDTO = new TaskDTO();
-		
+
 		taskDTO.id = task.getId();
 		taskDTO.name = task.getName();
 		taskDTO.finishDate = task.getFinishDate();
 		taskDTO.description = task.getDescription();
-		
+
 		return taskDTO;
 	}
-	
+
 }
--- src/main/java/com/task/list/model/Task.java
+++ src/main/java/com/task/list/model/Task.java
@@ -2,7 +2,6 @@ package com.task.list.model;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,14 +13,23 @@ import javax.persistence.TemporalType;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.task.list.dto.TaskDTO;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
-public class Task implements Serializable{
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Task implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String name;
 	@Temporal(TemporalType.DATE)
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", locale = "pt-BR", timezone = "Brazil/East")
@@ -31,70 +39,7 @@ public class Task implements Serializable{
 	private Date finishDate;
 	private String description;
 	private boolean finished;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public Date getStartDate() {
-		return startDate;
-	}
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
-	public Date getFinishDate() {
-		return finishDate;
-	}
-	public void setFinishDate(Date finishDate) {
-		this.finishDate = finishDate;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public boolean isFinished() {
-		return finished;
-	}
-	public void setFinished(boolean finished) {
-		this.finished = finished;
-	}
-	
-	
-	@Override
-	public String toString() {
-		return "TaskList [id=" + id + ", name=" + name + ", startDate=" + startDate + ", finishDate=" + finishDate
-				+ ", description=" + description + ", finished=" + finished + "]";
-	}
-	@Override
-	public int hashCode() {
-		return Objects.hash(description, finishDate, finished, id, name, startDate);
-	}
-	
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Task other = (Task) obj;
-		return Objects.equals(description, other.description) && Objects.equals(finishDate, other.finishDate)
-				&& finished == other.finished && id == other.id && Objects.equals(name, other.name)
-				&& Objects.equals(startDate, other.startDate);
-	}
-	
+
 	public void transformDTOtoModel(TaskDTO taskDTO) {
 		this.name = taskDTO.getName();
 		this.finishDate = taskDTO.getFinishDate();
@@ -102,5 +47,5 @@ public class Task implements Serializable{
 		this.startDate = new Date();
 		this.finished = false;
 	}
-	
+
 }
